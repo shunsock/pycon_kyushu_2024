bf7d3cca406ba82f2706ab448c2460f9125cee45
Shun <s.tsuchiya.business@gmail.com>
2024-05-18 14:13:41 +0900

    commentの編集

--- src/project/tests/hello/test_hello_wirh_typeguard.py
+++ src/project/tests/hello/test_hello_wirh_typeguard.py
@@ -10,14 +10,5 @@ def test_hello_with_typeguard() -> None:
 
 
 def test_hello_raise_error_with_invalid_input() -> None:
-    """
-    Pythonは動的型付け言語なので、型ヒントは実行時に評価されない。
-    そのため、型ヒントがstr型であるにも関わらずint型を入力してもエラーが発生しない。
-
-    そこで、この関数は、期待されていない型の入力値に対して、5行目でTypeCheckErrorを発生させる
-    ```python
-    check_type(name, str)
-    ```
-    """
     with pytest.raises(TypeCheckError):
         run(123)
