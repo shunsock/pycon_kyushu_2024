7619c210a992d3da414ed69209fd22ae100c72cd
Shun <s.tsuchiya.business@gmail.com>
2024-05-18 13:37:27 +0900

    commentの編集

--- src/project/tests/hello/test_hello_wirh_bug.py
+++ src/project/tests/hello/test_hello_wirh_bug.py
@@ -8,8 +8,7 @@ def test_hello_with_bug() -> None:
 
 def test_hello_does_not_raise_error_with_invalid_input() -> None:
     """
-    Pythonは動的型付け言語なので、型ヒントは実行時に評価されない。
-    そのため、型ヒントがstr型であるにも関わらずint型を入力してもエラーが発生しない。
+    型チェックがないので、int型を入力してもエラーが発生しない。
     この場合は、Castが発生している。
     """
     response = run(123)
