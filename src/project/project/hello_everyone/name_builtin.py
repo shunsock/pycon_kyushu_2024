2dbf5eb1f71414d7743387a2deb67b7e7e9b84be
Shun <s.tsuchiya.business@gmail.com>
2024-05-22 00:41:29 +0900

    NameのBuiltinだけで作ったバージョンを追加

--- src/project/project/hello_everyone/name_builtin.py
+++ src/project/project/hello_everyone/name_builtin.py
@@ -2,6 +2,9 @@ class NameBuiltin:
     value: str
 
     def __init__(self, value: str):
+        if isinstance(value, str) is False:
+            raise TypeError("Name must be a string")
+
         empty_name = len(value) < 1
         if empty_name:
             raise ValueError("Empty name")
