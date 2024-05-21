296120097c9d24a1ddc66d69090c957077433787
Shun <s.tsuchiya.business@gmail.com>
2024-05-22 00:21:43 +0900

    NameのValidationを修正

--- src/project/project/hello_everyone/name.py
+++ src/project/project/hello_everyone/name.py
@@ -6,11 +6,11 @@ class Name(BaseModel):
 
     @field_validator("value")
     def name_length(cls, value: str) -> str:
-        empty_name = len(value) == 0
+        empty_name = len(value) < 1
         if empty_name:
             raise ValueError("Empty name")
 
-        too_long_name = len(value) >= 100
+        too_long_name = len(value) > 100
         if too_long_name:
             raise ValueError("Too long name")
 
